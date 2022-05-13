8182eede218a9d93c9778e683acf7dc6baeddbaf
holynekk <e238109@metu.edu.tr>
2022-05-13 16:31:55 +0300

    latest_records function completed

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from django.db.models import F, Func, Value, CharField
+from django.db.models import Count
 from .models import NavigationRecord, Vehicle
 from .serializers import NavigationRecordSerializer, VehicleSerializer
 from .utils import create_random_plate
@@ -9,25 +9,24 @@ import random
 from django.utils import timezone
 
 
-"""
-The function that returns the list of last points per vehicle
-that have sent navigation data in the last  48 hours. 
-"""
 
 def latest_records():
+    """
+    The function that returns the list of last points per vehicle
+    that have sent navigation data in the last  48 hours.
+
+    It basically calculates 48 hours time range to use it in the query.
+    Then, filter the data with respect to calculated last2days variable.
+    At the end, sort the data and pick the lates records per vehicle.
+    """
+    # Get current time
     now = timezone.now()
-    # vehicle = Vehicle.objects.filter(id=15).first()
-    # for i in range(20):
-    #     past = now - datetime.timedelta(seconds=random.randint(0, 10*86400))
-    #     record = NavigationRecord.objects.create(vehicle=vehicle, latitude=round(random.uniform(1,50), 2), longitude=round(random.uniform(1,50), 2), datetime=past)
-    #     record.save()
+    # Calculate the exact time range
     last2days = now - datetime.timedelta(seconds=2*86400)
-    # print("---------------------------")
-    # print(NavigationRecord.objects.annotate(plate="vehicle"))
-    # print("---------------------------")
-    query_set = NavigationRecord.objects.filter(datetime__range = [last2days, now])
-    query_set_formatted = query_set.annotate(formatted_date=Func(F('datetime'), Value('DD-MM-YYYY HH:MM:SS'), function='to_char', output_field=CharField()))
-    return query_set_formatted
+    # Query set 
+    query_set = NavigationRecord.objects.filter(datetime__range = [last2days, now]).order_by('vehicle', '-datetime').distinct('vehicle')
+    # Return to view
+    return query_set
 
 class recent_records_view(generics.ListCreateAPIView):
     # Get the queryset by calling latest_records function
@@ -36,6 +35,6 @@ class recent_records_view(generics.ListCreateAPIView):
 
 
 class vehicles_view(generics.ListCreateAPIView):
-    queryset = Vehicle.objects.all()#.filter(plate__startswith="23")
+    queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
 
